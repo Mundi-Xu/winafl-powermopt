3bd3bd498a5840e439e73d54c8975d043e04edb4
Hanyu <43680784+Mundi-Xu@users.noreply.github.com>
2021-09-24 13:00:05 +0800

    Update cov.py

--- utils/cov.py
+++ utils/cov.py
@@ -10,7 +10,7 @@ import sys
 import os
 
 if len(sys.argv) != 2:
-    print("Usage:", sys.argv[0], "\corpus_path")
+    print("Usage:", sys.argv[0], "corpus_path")
 else:
     testcases = []
     folder = str(sys.argv[1])
@@ -18,7 +18,6 @@ else:
     for root, dirs, files in os.walk(folder, topdown=False):
         for name in files:
             if True:
-                # if name.endswith(".xml"):
                 testcase = os.path.abspath(os.path.join(root, name))
                 testcases.append(testcase)
                 sum += 1
@@ -28,6 +27,6 @@ else:
         print("\n[", i, "of", sum, "] Running DynamoRIO for testcase: ",
               testcase)
         os.system(
-            "E:\\fuzzer\\DynamoRIO-Windows-8.0.18864\\bin32\\drrun.exe -t drcov -- E:\\fuzzing\\msxml\\runtime_fuzz\\msxml.exe %s"
-            % testcase)
+            "DynamoRIO\\bin32\\drrun.exe -t drcov -- msxml.exe %s"
+            % testcase) # Remember to change this command to adapt
         i += 1
